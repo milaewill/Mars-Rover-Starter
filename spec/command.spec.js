7e134c0ccb75e4ca5d2c423b939765f431b6f106
Mila Williams <milaewill@Milas-MBP.lan>
2023-11-09 14:22:41 -0600

    TASK 1

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -3,10 +3,20 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Command class", function() {
+describe("Command class", function () {
 
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
-    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+  it("throws error if command type is NOT passed into constructor as the first parameter", function () {
+    expect(function () { new Command(); }).toThrow(new Error('Command type required.'));
+  });
+
+  it("constructor sets command type", () => {
+    const command = new Command('MODE_CHANGE', 'LOW_POWER');
+    expect(command.commandType).toBe('MODE_CHANGE');
+  });
+
+  it("constructor sets command value", () => {
+    const command = new Command('MODE_CHANGE', 'LOW_POWER');
+    expect(command.value).toBe('LOW_POWER');
   });
 
 });
